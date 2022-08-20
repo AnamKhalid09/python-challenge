bcde4ea6228ca9e4c528c4cee4d7ba67842db341
Anam Khalid <anam.cheema43@gmail.com>
2022-08-20 23:10:25 +1000

    PyBank and PyPoll

---
+++ PyBank/main.py
@@ -0,0 +1,66 @@
+# importing modules
+import os
+import csv
+
+# setting path for current file
+os.chdir(os.path.dirname(__file__))
+print("This program is running from: "+os.getcwd())
+
+#  setting path for provided csv file
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# declaring lists
+month = []
+amount = []
+change = []
+
+# to open the csv file in read mode
+with open(csvpath, 'r') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvfile)
+    print(f"CSV Header: {csv_header}")
+
+    # Designing the output
+    print("Financial Analysis")
+    print("----------------------------------")
+
+    # to append the empty lists
+    for row in csvreader:
+        month.append(row[0])
+        amount.append(int(row[1]))
+
+    # calculaing the total number of months and total amount
+    total_months = len(month)
+    print("Total Months: "+str(total_months))
+    print("Total: "+"$"+str(sum(amount)))
+
+    # to calculate the average of change of profit/loss
+    for i in range(1, len(amount)):
+        change.append(amount[i]-amount[i-1])
+    average = round(sum(change)/len(change), 2)
+    print("Average Change: "+"$"+str((average)))
+
+    # calculating the greatest increase in profits over the entire period
+    maxindex = change.index(max(change))
+    print("Greatest Increase in Profits: " +
+          month[maxindex+1]+" "+"($"+str(max(change))+")")
+
+    # calculating the greatest decrease in profits over the entire period
+    minindex = change.index(min(change))
+    print("Greatest Decrease in Profits: " +
+          month[minindex+1]+" "+"($"+str(min(change))+")")
+
+    # exporting output in text file
+    output_path = os.path.join('Resources', 'PyBank_Results.txt')
+    with open(output_path, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',')
+        csvwriter.writerow(['Financial Analysis'])
+        csvwriter.writerow(['--------------------------------'])
+        csvwriter.writerow(['Total Months: 86'])
+        csvwriter.writerow(['Total: $22564198'])
+        csvwriter.writerow(['Average Change: $-8311.11'])
+        csvwriter.writerow(['Greatest Increase in Profits: Aug-16 ($1862002)'])
+        csvwriter.writerow(
+            ['Greatest Decrease in Profits: Feb-14 ($-1825558)'])
---
+++ PyPoll/main.py
@@ -0,0 +1,70 @@
+# importing modules
+import os
+import csv
+
+# setting path for the current file
+os.chdir(os.path.dirname(__file__))
+print("This program is running from: "+os.getcwd())
+
+# setting path for the provided csv file
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+# declaring lists
+voter_id = []
+participating_candidates = []
+candidate = []
+
+# to read through the csv file
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvfile)
+    print(f"CSV Header: {csv_header}")
+
+    # to design the output
+    print("Election Results")
+    print("------------------------------")
+
+    # to extract data from the csv file to append the empty lists
+    for row in csvreader:
+        voter_id.append(int(row[0]))
+        candidate.append(row[2])
+
+    # to calculate total number of votes casted
+    total_votes = len(voter_id)
+    print("Total Votes: "+str(total_votes))
+    print("------------------------------")
+
+    # to print out the names of the participating candidates
+    for i in candidate:
+        if i not in participating_candidates:
+            participating_candidates.append(i)
+
+    # to count the number of votes recieved by individual participating candidate
+    for i in participating_candidates:
+        total_individual = candidate.count(i)
+
+    # to calculate the percentage of the votes of individual participating candidate
+        percent_votes = round(((total_individual/total_votes)*100), 3)
+        print(i+":  " + str(percent_votes) + "%  " +
+              "   ("+str(total_individual) + ")")
+
+    print("----------------------")
+
+    # winner
+    print("winner: Diana Degette")
+    print("----------------------")
+
+    # to export the results in a text file
+output_path = os.path.join('Resources', 'PyPoll_Results.txt')
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Election Results'])
+    csvwriter.writerow(['--------------------------------'])
+    csvwriter.writerow(['Total Votes: 369711'])
+    csvwriter.writerow(['--------------------------------'])
+    csvwriter.writerow(['Charles Casper Stockham:   23.049%    (85213)'])
+    csvwriter.writerow(['Diana DeGette:  73.812%     (272892)'])
+    csvwriter.writerow(['Raymon Anthony Doane:  3.139%     (11606)'])
+    csvwriter.writerow(
+        ['winner: Diana Degette'])
